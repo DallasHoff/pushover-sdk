e9df5a118c894834ee8f2cac85eabebb1a385ef8
Dallas Hoffman <dallashoff@gmail.com>
2025-09-01 23:17:18 -0400

    Add base error class

--- src/errors.ts
+++ src/errors.ts
@@ -1,8 +1,18 @@
 /**
+ * The base class for all errors that this SDK might throw.
+ */
+export class PushoverError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'PushoverError';
+	}
+}
+
+/**
  * An error that is thrown when the Pushover API responds with an
  * error status.
  */
-export class PushoverResponseError extends Error {
+export class PushoverResponseError extends PushoverError {
 	constructor(message: string) {
 		super(message);
 		this.name = 'PushoverResponseError';
@@ -13,7 +23,7 @@ export class PushoverResponseError extends Error {
  * An error that is thrown when the parameters passed to the
  * Pushover API are invalid in some way.
  */
-export class PushoverParameterError extends Error {
+export class PushoverParameterError extends PushoverError {
 	constructor(message: string) {
 		super(message);
 		this.name = 'PushoverParameterError';
